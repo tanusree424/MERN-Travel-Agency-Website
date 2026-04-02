0afa4e27baec0646f93c9c0d45ba27dbf91e1afb
Tanusree Basu Choudhury <tbasuchoudhury@gmail.com>
2026-04-02 14:44:42 +0530

    changed BASEURL

--- frontend/src/Api/Api.js
+++ frontend/src/Api/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL:"https://travel-agency-website-nqre.onrender.com",
+   baseURL:"https://travel-agency-website-nqre.onrender.com/",
   // baseURL:"http://localhost:5000",
     withCredentials:true
 });
