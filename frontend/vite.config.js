8822b7ec76df8bc638b8fc408a25e39c43b3d7e1
Tanusree Basu Choudhury <tbasuchoudhury@gmail.com>
2026-04-02 12:39:02 +0530

    Docker initialize successfully

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,3 +7,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
    
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
